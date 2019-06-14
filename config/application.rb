6db84b5718601269a3251c1c31b3cbe06a8413b7
hiendinhngoc <hiendinhngoc@yahoo.com.vn>
2019-06-14 13:52:56 +0700

    Add rspec test

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,8 @@ module DC
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
